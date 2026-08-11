1e3b427e82e795b0b9827c328ed033574cae5e40
sarveshkv <sarveshkannan123@gmail.com>
2026-08-11 15:20:42 +0530

    Setup.py Ready

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    """Reads the requirements from a file and returns them as a list."""
+    requirement_lst:List[str] = []
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and  requirement!='-e .':
+                    requirement_lst.append(requirement)
+    except Exception as e:
+        print(f"Error occurred while reading requirements from {file_path}: {e}")
+
+    return requirement_lst
+print(get_requirements("requirements.txt"))
+
+setup(
+    name="network_security",
+    version="0.0.0",
+    author="Sarvesh",
+    author_email="sarveshkannan123@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
